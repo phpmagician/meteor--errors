8b95eb53bd228a80589d547b0baec354fb92317a
Ranjith Sivaraman <rsivaraman2@summitsta.com>
2015-04-26 19:09:46 +0530

    initial changes

---
+++ errors.js
@@ -0,0 +1,10 @@
+// Write your package code here!
+Errors = {
+// Local (client-only) collection
+		collection: new Mongo.Collection(null),
+		throw: function(message) {
+			
+		Errors.collection.insert({message: message, seen: false})
+
+		}
+};
